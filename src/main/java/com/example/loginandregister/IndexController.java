1aca4ad383396a33519ef5c7858190ea112265ec
RAStoychev18 <RAStoychev18@codingburgas.bg>
2022-11-21 10:40:56 +0200

    Initial commit

---
+++ src/main/java/com/example/loginandregister/IndexController.java
@@ -0,0 +1,21 @@
+package com.example.loginandregister;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+    @PostMapping("/register")
+    public String userRegistration(@ModelAttribute User user, Model model){
+        System.out.println(user.toString());
+        return "welcome";
+    }
+}
